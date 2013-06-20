c15a5634800c113d28d03564822fd9a6c2128dde
Nicolas <rudas.n@gmail.com>
2013-06-20 17:56:09 +0300

    todomvc

--- todomvc/todomvc.js
+++ todomvc/todomvc.js
@@ -5,15 +5,17 @@ var Todo = Voodoo.Model.create({
     'defaults': {
         'text': 'Untitled',
         'is_done': false,
-        'is_active': true
+        'is_deleted': false
     }
 });
 
 // A collection of todo item models.
 var Todos = Voodo.Store.create({
-    'model': Todo, // link this collection with the Todo model
     'parse': function(response, status, xhr) {
         return response.items || [];
+    },
+    'parseItem': function(item){
+        return new Todo(item);
     }
 });
 
@@ -21,23 +23,49 @@ var Todos = Voodo.Store.create({
 var TodoView = Voodo.View.create({
     // We are using a template to construct the HTML.
     'template': '#todo-template',
+    // Get the result of this.key.
+    // It should evaluate to a DOM element or empty string.
+    // If a dom element, replace.
+    // If a string, and can append, append do it.
+    // If a string, and can set value attr do it.
+    // If a boolean, and can check|uncheck|select|unselect do it.
+    // If a voodoo instance, go through ghosts and render and return
+    // the first view instance (if any).
+    // If a store, go through each item and parse that.
+    // Else hide.
     'areas': {
-        // One-way data-binding.
-        '.todo-text': 'model.text',
         // Two-way data-binding.
-        // If the value  changes the checkbox changes. If the
-        // checkbox changes the value changes.
+        // The DOM element ".todo-text" holds the result of
+        // this.model.text. If model.text changes the html changes.
+        '.todo-text': 'model.text',
+        // We want an input to be able to update model.text.
+        '.todo-text-edit': 'model.text',
+        // If model.is_done evaluates to true the checkbox is checked.
+        // If the checkbox changes model.is_done also changes.
         '.todo-toggle-done': 'model.is_done',
     },
     'classNameBindings': {
-        // Add/Remove class names depending on some properties.
-        'model.is_done': 'is-done:is-not-done',
-        'model.is_active': ':hidden'
+        // Done items are displayed differently than active items.
+        // We also need to distinguish the two when filtering by status.
+        'model.is_done': 'is-done:is-active',
+        // Deleted items are not displayed.
+        'model.is_deleted': 'hidden',
+        // When editing the item we need to show the text input
+        // and hide other things.
+        'is_editing': 'is-editing'
     },
     'events': {
-        // Basic jQuery events.
+        // Delete.
         'click .todo-delete': function(e, $target) {
-            this.set('is_active', !this.get('is_active'));
+            this.model.is_deleted = !this.model.is_deleted;
+        },
+        // Edit (on).
+        'dblclick .todo-text': function(e, $target) {
+            this.is_editing = true;
+        },
+        // Edit (off).
+        'blur .todo-text-edit': function(e, $target) {
+            this.is_editing = false;
         }
     }
 });
@@ -45,22 +73,19 @@ var TodoView = Voodo.View.create({
 var TodosView = Voodo.View.create({
     'template': '#todos-template',
     'areas': {
-        '.todos-list': {
-            // Bind a callback to the
-            'collection': function(el, collection) {
-                collection.forEach(function(item) {
-                    var view = new TodoView({
-                        'delegateEventsTo': this
-                    });
-                    view.set('model', item);
-                    el.append(item.view.render());
-                });
-                return el;
-            }
-        },
-        '.todos-list-count-number': 'active.length'
+        // The .todos-list element holds all the todo items on our collection,
+        // a store instance.
+        // When a new item is added in the collection it gets added to the
+        // DOM. When an item is removed it gets removed from the DOM.
+        '.todos-list': 'collection',
+        '.todos-list-count-number': 'collection_active.length'
     },
     'classNameBindings': {
+        // Our filter_by property is added as a class on the view element
+        // so that we can show/hide elements depending on their state
+        // (done|active|all).
+        // So that, .filter-by-done .is-active,
+        //          .filter-by-active .is-done { display: none; }
         'filter_by': 'filter-by-{{ filter_by }}'
     },
     'events': {
@@ -69,30 +94,55 @@ var TodosView = Voodo.View.create({
             this.filter(change.to);
         }
     },
+    // Filter our todos based on their status.
+    // We are only changing a property on the view here.
+    // The className binding on filter_by property will change
+    // the class name of this view to include the type.
+    // CSS then takes care of which items will be visible and which not.
     'filter': function(type) { // type: 'done'|'all'|'active'
         if (type !== 'done' && type !== 'active') {
             type = 'all';
         }
-        // We are only changing a property on the view here.
-        // The className binding on filter_by property will change
-        // the class name of this view to include the type.
-        // CSS then takes care of which items will be visible and which not.
-        this.set('filter_by', type);
+        this.filter_by = type;
         return this;
     },
     'render': function() {
-        var collection = this.get('collection'),
-            active_items = this.get('collection').filter({
+        var collection = this.collection,
+            active_items = collection.filter({
             'is_done': false
-        });
+        }).live();
         // "Live" collections.
-        // When the collection changes (ie, one of its models changes
-        // it's is_done property) the collection removes that item
-        // and triggers a change event on 'this.active'.
-        this.set('active', active_items);
+        // Adds listeners to property changes of the parent collection
+        // (in this case is_done) and if the property changes it either
+        // adds or removes changed item from the returned collection
+        // and triggers a change event from it.
+        this.collection_active = active_items;
         this.html(this.template()); // does areas as well
         this.filter();
         return this;
+    }
+});
+
+Voodoo.Routes.add('/', {
+    'enter': function(url) {
+        var todos, todos_view;
+        todos = this.todos = new Todos(url + '.json');
+        todos_view = this.todos_view = new TodosView();
+        // For each item create another instance.
+        // a shortcut to on:initialize listener.
+        todos.ghost(TodoView, function(item, constr, collection) {
+            return new constr(item, {
+                'delegateEventsTo': todos_view
+            });
+        });
+        todos_view.collection = todos;
+        todos_view.render();
+        scope.app_view.views.todos = todos_view;
+    },
+    'exit': function(url) {
+        scope.app_view.views.todos = null;
+        this.todos_view.collection = null;
+        this.todos_view.destroy();
     }
 });
 
@@ -100,48 +150,33 @@ Voodoo.Routes.add('/todo/:id', {
     'enter': function(url, id) {
         this.todo = new Todo(url + '.json');
         this.todo = new TodoView();
-        this.todo_view.set('model', this.todo);
-        scope.app_view.set('views.todo', this.todo_view);
+        this.todo_view.model = this.todo;
         this.todo_view.render();
+        scope.app_view.views.todo = this.todo_view;
     },
     'exit': function(url,id) {
-        this.todo_view.unset('model', this.todo);
-        scope.app_view.unset('views.todo', this.todo_view);
+        this.todo_view.model = null;
+        scope.app_view.views.todo = null;
         this.todo_view.destroy();
         this.todo.destroy();
     }
 });
 
-Voodoo.Routes.add('/', {
-    'enter': function(url) {
-        this.todos = new Todos(url + '.json');
-        this.todos_view = new TodosView();
-        this.todos_view.set('collection', this.todos);
-        scope.app_view.set('views.todos', this.todos_view);
-        this.todos_view.render();
-    },
-    'exit': function(url) {
-        this.todos_view.unset('collection', this.todos);
-        scope.app_view.unset('views.todos', this.todos_view);
-        this.todos_view.destroy();
-    }
-});
-
 Voodoo.Routers.add('/about/', {
     'enter': function(url) {
-        scope.app_view.set('text.about', '<p>About us!</p>');
+        scope.app_view.area('#about-us .about-us').show();
     },
-    'exit': function(url) {
-        scope.app_view.unset('text.about');
+    'exit, drill': function(url) {
+        scope.app_view.area('#about-us .about-us').hide();
     }
 });
 
 Voodoo.Routers.add('/about/contact', {
     'enter': function(url) {
-        scope.app_view.set('text.contact', $('#contact-us'));
+        scope.app_view.text.contact = $('#contact-us');
     },
     'exit': function(url) {
-        scope.app_view.unset('text.contact');
+        scope.app_view.text.contact = null;
     }
 });
 
@@ -151,8 +186,18 @@ Voodoo.Routes.initialize(function() {
         'areas': {
             '#todo': 'views.todo',
             '#todos': 'views.todos',
-            '#about-us': 'text.about',
-            '#contact-us': 'text.contact'
-        }
+            '#about-us .about-us': 'text.about',
+            '#about-us .contact-us': 'text.contact'
+        },
+        'text': {
+            'about': '<p>About us!</p>'
+        },
+        'views': {}
     });
 });
+
+// A list of models
+// create a view for each one of them
+// and sort & display each view
+// on a particular DOM element.
+// When list changes, the DOM changes.
